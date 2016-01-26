6d503b0744b0031a9403f8b69f890df13f2e39b8
Tiago Murakami <trmurakami@gmail.com>
2016-01-26 08:34:11 -0200

    Migração para o bootstrap4

--- catalog_framework.php
+++ catalog_framework.php
@@ -2,15 +2,9 @@
 <head>
 <title>Framework de Catalogação SIBiUSP - Catalogação</title>
 
-<!-- Jquery -->
-<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
+<?php
+  include "inc/meta_header.php";
+?>
 </head>
 <body>
   <div class="container">
@@ -18,8 +12,6 @@
     <?php
       include "inc/header.php";
     ?>
-
-
 
   </div>
 </body>
--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,7 @@
 <!-- CI-Explorer - Cabeçalho -->
   <div class="row">
     <div class="col-md-8">
-      <div class="page-header">
         <h1><a href="index.php">Framework de Catalogação SIBiUSP</a> <small></small></h1>
-      </div>
     </div>
     <div class="col-md-4"><br/><p>Universidade de São Paulo</p></div>
   </div>
